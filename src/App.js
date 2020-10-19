fc00e56c4a5b58f579527d8adde3ce51e93c784e
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-10-19 12:41:46 -0400

    Ouvre et ferme le panier

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ export default class App {
 	 * Méthode principale. Sera appelée après le chargement de la page.
 	 */
 	static main() {
-		console.log("Je suis prêt");
 		var app = document.getElementById("app");
-		// app.innerHTML = "La page est chargée";
+		var lienPanier = document.getElementById("lien-panier");
+		lienPanier.addEventListener("click", e => {
+			document.body.classList.toggle("panier");
+		});
+		var close = document.querySelector("#panier > .close");
+		close.addEventListener("click", e => {
+			document.body.classList.toggle("panier");
+		});
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
