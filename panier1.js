dfabc7382a073e21523c4220e6a86f2079d7cd4a
Martin Boudreau <mboudrea\cstj.qc.ca>
2023-10-02 07:17:13 -0400

    2023

--- panier1.js
+++ panier1.js
@@ -1,7 +1,7 @@
 export default {
     achats: [
-        {numero: "K2qN68Pg6TI", quantite: 1},
-        {numero: "ZCoqcrWp9GY", quantite: 30}
+        { numero: "K2qN68Pg6TI", quantite: 1 },
+        { numero: "ZCoqcrWp9GY", quantite: 30 }
     ],
     produits: {
         "PlkN_pN2ryc": {
@@ -38,6 +38,77 @@ export default {
             titre: "Une personnalité",
             description: "Vous n'avez pas de personnnalité, c'est bien connu. Prenez donc des cours!",
             prix: 52
+        },
+        "X9qAePmK7Zs": {
+            "numero": "X9qAePmK7Zs",
+            "image": "images/produits/X9qAePmK7Zs.jpg",
+            "titre": "Machine à rêves",
+            "description": "Cette machine révolutionnaire peut transformer vos rêves en réalité. Elle est si incroyable qu'elle pourrait probablement même résoudre la crise existentielle de Sartre.",
+            "prix": 999.99
+        },
+        "R4wN3sTlE7t": {
+            "numero": "R4wN3sTlE7t",
+            "image": "images/produits/R4wN3sTlE7t.jpg",
+            "titre": "Robo-Chef",
+            "description": "Un robot de cuisine avancé qui peut cuisiner n'importe quel plat en un instant. Il est tellement doué qu'il ferait pleurer Gordon Ramsay de jalousie.",
+            "prix": 299.50
+        },
+        "M1gD3tVp5Qo": {
+            "numero": "M1gD3tVp5Qo",
+            "image": "images/produits/M1gD3tVp5Qo.jpg",
+            "titre": "Lampe Arc-en-ciel",
+            "description": "Illuminez votre espace avec cette lampe magique qui projette un arc-en-ciel en 3D dans toute la pièce. C'est tellement spectaculaire que même les licornes en sont jalouses.",
+            "prix": 49.95
+        },
+        "B8kPvWzN6Yj": {
+            "numero": "B8kPvWzN6Yj",
+            "image": "images/produits/B8kPvWzN6Yj.jpg",
+            "titre": "Sac à dos volant",
+            "description": "Ce sac à dos équipé de propulseurs vous permettra de voler à travers le ciel. Idéal pour les aventuriers intrépides qui pensent que la gravité est pour les faibles.",
+            "prix": 799.99
+        },
+        "T3nGcH4tV7R": {
+            "numero": "T3nGcH4tV7R",
+            "image": "images/produits/T3nGcH4tV7R.jpg",
+            "titre": "Plante parlante",
+            "description": "Cette plante d'intérieur a la capacité de tenir des conversations intelligentes. Elle est tellement intelligente qu'elle pourrait probablement remporter un débat contre Einstein.",
+            "prix": 39.99
+        },
+        "Y7uKpLmQ1Wv": {
+            "numero": "Y7uKpLmQ1Wv",
+            "image": "images/produits/Y7uKpLmQ1Wv.jpg",
+            "titre": "Montre du Voyageur Temporel",
+            "description": "Cette montre vous permet de voyager dans le temps et de vivre des événements historiques en personne. Pff, comme si les autres montres pouvaient faire ça ! (Avertissement : Les voyages dans le temps ne sont pas garantis.)",
+            "prix": 1999.99
+        },
+        "N0sF6rE2vHm": {
+            "numero": "N0sF6rE2vHm",
+            "image": "images/produits/N0sF6rE2vHm.jpg",
+            "titre": "Cape d'Invisibilité",
+            "description": "Devenez invisible à volonté avec cette cape magique. Les autres vêtements sont-ils aussi utiles que ça ? Je ne pense pas ! (Note : Effets d'invisibilité peuvent varier.)",
+            "prix": 499.50
+        },
+        "Q5mT2oPv3Xr": {
+            "numero": "Q5mT2oPv3Xr",
+            "image": "images/produits/Q5mT2oPv3Xr.jpg",
+            "titre": "Casque de Lecture Mentale",
+            "description": "Portez ce casque pour lire les pensées et avoir un aperçu des pensées de ceux qui vous entourent. Oui, vous avez bien lu, vous pouvez maintenant être un télépathe en herbe ! (Note : La lecture mentale n'est pas garantie.)",
+            "prix": 299.95
+        },
+        "A8bV1wZrNpO": {
+            "numero": "A8bV1wZrNpO",
+            "image": "images/produits/A8bV1wZrNpO.jpg",
+            "titre": "Chaussures Défiant la Gravité",
+            "description": "Ces chaussures vous permettent de défier la gravité et de marcher sur les murs et les plafonds. Oui, vous pouvez être un super-héros maintenant. (Note : N'essayez pas de gravir des montagnes avec ces chaussures.)",
+            "prix": 599.99
+        },
+        "E3qRvGpI7Xm": {
+            "numero": "E3qRvGpI7Xm",
+            "image": "images/produits/E3qRvGpI7Xm.jpg",
+            "titre": "Projecteur Holographique Portable",
+            "description": "Transportez ce dispositif portable et projetez des images et des vidéos holographiques où que vous alliez. Impressionnez vos amis et votre famille ! (Note : Ne convient pas pour les projections sur la lune.)",
+            "prix": 79.99
         }
     }
-}
+
+};
---
+++ panier2 copy.js
@@ -0,0 +1,44 @@
+export default {
+    achats: [
+        {numero: "PlkN_pN2ryc", quantite: 1},
+        {numero: "FhWCdE9qpN0", quantite: 1},
+        {numero: "V58Dl1fv42c", quantite: 3}
+    ],
+    produits: {
+        "PlkN_pN2ryc": {
+            numero: "PlkN_pN2ryc",
+            image: "images/produits/PlkN_pN2ryc.jpg",
+            titre: "Animal de compagnie",
+            description: "Ce couple d'escargots vous apportera des heures de bonheur.",
+            prix: 100
+        },
+        "FhWCdE9qpN0": {
+            numero: "FhWCdE9qpN0",
+            image: "images/produits/FhWCdE9qpN0.jpg",
+            titre: "Du talent",
+            description: "Vous croyez ne pas avoir de talent? Vous avez raison. Avec cet établi, vous pourrez vous exercer en toute sécurité.",
+            prix: 1425
+        },
+        "K2qN68Pg6TI": {
+            numero: "K2qN68Pg6TI",
+            image: "images/produits/K2qN68Pg6TI.jpg",
+            titre: "Divertissement",
+            description: "Cette télévision \"vintage\" est la touche qu'il manquait à votre salon.",
+            prix: 14.50
+        },
+        "V58Dl1fv42c": {
+            numero: "V58Dl1fv42c",
+            image: "images/produits/V58Dl1fv42c.jpg",
+            titre: "Voyagez!",
+            description: "Cette magnifique coccinelle est offerte en location à la journée. Le tricycle est compris.",
+            prix: 35
+        },
+        "ZCoqcrWp9GY": {
+            numero: "ZCoqcrWp9GY",
+            image: "images/produits/ZCoqcrWp9GY.jpg",
+            titre: "Une personnalité",
+            description: "Vous n'avez pas de personnnalité, c'est bien connu. Prenez donc des cours!",
+            prix: 52
+        }
+    }
+}
